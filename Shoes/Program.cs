0a0d3e920eec179b683a3187ae08235958e415e8
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-03 21:51:57 +0200

    Added Main method

--- Shoes/Program.cs
+++ Shoes/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        static ShoeCloset shoeCloset = new ShoeCloset();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                shoeCloset.PrintShoes();
+                Console.Write("\nWciśnij 'd', aby dodać, lub 'u', aby usunąć buty: ");
+                char key = Console.ReadKey().KeyChar;
+
+                switch (key)
+                {
+                    case 'd':
+                    case 'D':
+                        shoeCloset.AddShoe();
+                        break;
+                    case 'u':
+                    case 'U':
+                        shoeCloset.RemoveShoe();
+                        break;
+                    default:
+                        return;
+                }
+            }
         }
     }
 }
